63c8a707288e9088fef90d221520572c5b902a35
Homura <17104706+ototashkent@users.noreply.github.com>
2020-04-28 01:48:10 -0400

    forgot the require

--- generator.js
+++ generator.js
@@ -7,7 +7,7 @@ require(`${__dirname}/scripts/install_packages.js`);
 
 // serve data
 let command = `node ${__dirname}/node_modules/docsify-cli/bin/docsify serve ./docs --port 9505`
-if (os.platform() === "win32") command = `node "${__dirname}/node_modules/docsify-cli/bin/docsify" serve ./docs --port 9505`;
+if (require("os").platform() === "win32") command = `node "${__dirname}/node_modules/docsify-cli/bin/docsify" serve ./docs --port 9505`;
 
 console.log(chalk.hex("#66d9ff")("Running Command: ") +command);
 let docsProcess = childProcess.exec(command);
