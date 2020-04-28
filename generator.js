f1bfaa6f6bed306c53c1ad5e193890f96e5dd513
Homura <17104706+ototashkent@users.noreply.github.com>
2020-04-27 22:27:19 -0400

    thanks alten for having a space in your folder names like an idiot

--- generator.js
+++ generator.js
@@ -6,8 +6,8 @@ const chalk = require("chalk");
 require(`${__dirname}/scripts/install_packages.js`);
 
 // serve data
-console.log(chalk.hex("#66d9ff")("Running Command: ") +`node ${__dirname}/node_modules/docsify-cli/bin/docsify serve ./docs --port 8080`);
-let docsProcess = childProcess.exec(`node ${__dirname}/node_modules/docsify-cli/bin/docsify serve ./docs --port 8080`);
+console.log(chalk.hex("#66d9ff")("Running Command: ") +`node "${__dirname}/node_modules/docsify-cli/bin/docsify" serve ./docs --port 8080`);
+let docsProcess = childProcess.exec(`node "${__dirname}/node_modules/docsify-cli/bin/docsify" serve ./docs --port 8080`);
 
 docsProcess.on("exit", () => {
     process.exit(0);
