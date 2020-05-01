1cdacd56a1430c425491efc39a182c3d5991389f
SMJSGaming <sjoerd.t@live.nl>
2020-05-01 12:36:07 +0200

    some small cleanups

--- generator.js
+++ generator.js
@@ -1,8 +1,8 @@
 /* eslint no-console: ["error", { allow: ["log", "warn", "error"] }] */
 const childProcess = require("child_process");
-require(`${__dirname}/scripts/install_packages.js`); // install packages
-
 const chalk = require("chalk");
+
+require(`${__dirname}/scripts/install_packages.js`); // install packages
 
 // serve data
 let command = `node ${__dirname}/node_modules/docsify-cli/bin/docsify serve ./docs --port 9505`;
@@ -11,10 +11,8 @@ if (require("os").platform() === "win32") {
     command = `node "${__dirname}/node_modules/docsify-cli/bin/docsify" serve ./docs --port 9505`;
 }
 
-console.log(chalk.hex("#66d9ff")("Running Command: ") + command);
+console.log(`${chalk.hex("#66d9ff")("Running Command: ")} ${command}\n`);
 
 const docsProcess = childProcess.exec(command);
-
-console.log("");
 
 docsProcess.on("exit", () => process.exit(0));
