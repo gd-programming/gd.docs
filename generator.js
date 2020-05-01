a5d187cef3aafedacb4bb378f2ee1ea3e0352549
SMJSGaming <sjoerd.t@live.nl>
2020-05-01 10:21:29 +0200

    Made the pull process slower to make sure that it won't be seen as spam

--- generator.js
+++ generator.js
@@ -6,12 +6,15 @@ const chalk = require("chalk");
 
 // serve data
 let command = `node ${__dirname}/node_modules/docsify-cli/bin/docsify serve ./docs --port 9505`;
+
 if (require("os").platform() === "win32") {
     command = `node "${__dirname}/node_modules/docsify-cli/bin/docsify" serve ./docs --port 9505`;
 }
 
 console.log(chalk.hex("#66d9ff")("Running Command: ") + command);
+
 const docsProcess = childProcess.exec(command);
+
 console.log("");
 
 if (process.argv[2] == "--prod") {
--- scripts/git_pull.js
+++ scripts/git_pull.js
@@ -2,4 +2,4 @@ const chalk = require("chalk");
 setInterval(() => {
     require("child_process").execSync("git pull origin master");    
     console.log(chalk.hex("#66d9ff")("Fetching latest Git changes..."));
-}, 5000);
+}, 300000);
