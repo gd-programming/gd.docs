873616c7dfd2a2915c14f272a233ec4f87a648f7
SMJSGaming <sjoerd.t@live.nl>
2020-05-01 12:47:30 +0200

    case fixes and cleanups

--- generator.js
+++ generator.js
@@ -1,14 +1,15 @@
 /* eslint no-console: ["error", { allow: ["log", "warn", "error"] }] */
 const childProcess = require("child_process");
 const chalk = require("chalk");
+const os = require("os");
 
-require(`${__dirname}/scripts/install_packages.js`); // install packages
+require("./scripts/installPackages.js"); // install packages
 
 // serve data
-let command = `node ${__dirname}/node_modules/docsify-cli/bin/docsify serve ./docs --port 9505`;
+let command = "node ./node_modules/docsify-cli/bin/docsify serve ./docs --port 9505";
 
-if (require("os").platform() === "win32") {
-    command = `node "${__dirname}/node_modules/docsify-cli/bin/docsify" serve ./docs --port 9505`;
+if (os.platform() === "win32") {
+    command = "node \"./node_modules/docsify-cli/bin/docsify\" serve ./docs --port 9505";
 }
 
 console.log(`${chalk.hex("#66d9ff")("Running Command: ")} ${command}\n`);
---
+++ scripts/installPackages.js
@@ -0,0 +1,14 @@
+// install_packages.js
+// made by Homura
+const chalk = require("chalk");
+const fs = require("fs");
+const childProcess = require("child_process");
+
+const package = require("../package.json");
+const missing = Object.keys(Package.dependencies)
+    .filter(!fs.existsSync(`${__dirname}/../node_modules/${package}`));
+
+if (missing.length > 0) {
+    console.log(chalk.hex("#79b0fc")("Installing Packages: ") + missing.join(", "));
+    childProcess.exec(`npm i --save ${missing.join(" ")}`);
+}
